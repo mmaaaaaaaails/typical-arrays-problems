01ac2f63271cd3ff09afda261e84ca900898329b
Vadim <v.dudko93@gmail.com>
2020-02-25 19:08:32 +0300

    feat: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+    return 0;
+} else{
+    return Math.min(...array);
+  }
 }
-
 exports.max = function max (array) {
+if (array === undefined || array.length === 0){
   return 0;
+} else {
+    return Math.max(...array);
+  }
 }
-
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined|| array.length === 0){
+    return 0;
+  } else {
+    return array.reduce((total, amount) => (total + amount)) / array.length;
+  }
 }
